e9ec62982f5800d861c38d185e8e2c69a5009bee
b00baleh <aluke.oe@gmail.com>
2019-05-29 23:13:01 +0300

    -

---
+++ Select/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Select
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var l = new List<string>();
+            l.Add("abc");
+            l.Add("def");
+            l.Add("ghi");
+
+            var z = l.MySelect(x => x[0]).Take(2).ToArray();
+            /*
+            var myclass = new MyIenumClass<int>(l);
+
+            foreach (var i in myclass)
+            {
+
+            }
+            */
+        }
+    }
+}
---
+++ Select/Select.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Select
+{
+    public static class Select
+    {
+        public static IEnumerable<TResult> MySelect<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector)
+        {
+            var en = new MyIenumClass<TSource, TResult>(source, selector);
+
+            return (IEnumerable<TResult>)en;
+        }
+    }
+
+    public class MyIenumClass<TSource, TResult> : IEnumerable<TSource>
+    {
+        private IEnumerable<TSource> _list;
+        private Func<TSource, TResult> _selector;
+        public MyIenumClass(IEnumerable<TSource> input, Func<TSource, TResult> selector)
+        {
+            _list = input;
+            _selector = selector;
+        }
+
+        public IEnumerator<TSource> GetEnumerator()
+        {
+            return new MyEnumerator<TSource, TResult>(_list, _selector);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class MyEnumerator<TSource, TResult> : IEnumerator<TSource>
+    {
+        private IEnumerable<TSource> _objects;
+        private Func<TSource, TResult> _selector;
+        //private int _pos = -1;
+
+        public MyEnumerator(IEnumerable<TSource> input, Func<TSource, TResult> selector)
+        {
+            _objects = input;
+            _selector = selector;
+        }
+
+        public void Dispose()
+        {
+            _objects.GetEnumerator().Dispose();
+        }
+
+        public bool MoveNext()
+        {
+            Console.WriteLine("+");
+            
+            return (_objects.GetEnumerator().MoveNext());
+        }
+
+        public void Reset()
+        {
+            _objects.GetEnumerator().Reset();
+        }
+
+        public TSource Current
+        {
+            get { return _objects.GetEnumerator().Current; }
+        }
+
+        object IEnumerator.Current
+        {
+            get { return _selector(_objects.GetEnumerator().Current); }
+        }
+    }
+}
